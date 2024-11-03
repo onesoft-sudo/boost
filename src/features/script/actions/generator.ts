8c0797ec047d433e99584721627fffe7f5bf0bd0
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-11-03 18:20:33 +0000

    feat: basic app install script logic
    Signed-off-by: GitHub <noreply@github.com>

--- src/features/script/actions/generator.ts
+++ src/features/script/actions/generator.ts
@@ -2,15 +2,57 @@
 
 import "server-only";
 
+function getBanner() {
+    return `  
+ ____                  _   
+|  _ \\                | |  
+| |_) | ___   ___  ___| |_ 
+|  _ < / _ \\ / _ \\/ __| __|
+| |_) | (_) | (_) \\__ \\ |_ 
+|____/ \\___/ \\___/|___/\\__|
+    `;
+}
+
 export async function generateInstallScript(apps: string[]): Promise<string> {
     let script = "#!/bin/sh\n\n";
 
+    script += `me=$(basename "$0");\n`;
+    script += 'user="$(whoami)";\n';
+
+    script += 'if [ "$user" != "root" ]; then\n';
+    script +=
+        '    echo "$me: This script must be run as superuser to install apps" 2>&1;\n';
+    script += "    exit 1;\n";
+    script += "fi\n\n";
+
+    script += "cat <<EOF\n";
+    script += getBanner();
+    script += "\nEOF\n\n";
+
     for (const app of apps) {
-        script += `echo "Installing ${app}"\n`;
+        script += `echo "$me: Installing app: ${app}";\n`;
+        const code = getAppInstallCode(app);
+
+        if (!code) {
+            script += `echo "$me: Invalid app: ${app}";\n`;
+        } else {
+            script += code;
+        }
     }
 
     script += "\n";
-    script += 'echo "All done!"\n';
+    script += 'echo "All done!";\n';
 
     return script;
+}
+
+function getAppInstallCode(app: string): string | null {
+    switch (app) {
+        case "node":
+            return "curl -fsSL https://deb.nodesource.com/setup_22.x | bash - && apt-get install -y nodejs";
+        case "docker":
+            return "curl -fsSL https://get.docker.com | bash -";
+        default:
+            return null;
+    }
 }
